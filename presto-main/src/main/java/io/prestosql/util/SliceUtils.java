e7e4793ef626b1768dfe203a8fa25e09c7074cdd
artiship <meilziner@gmail.com>
2019-05-09 19:47:06 +0800

    fix code style checking error

--- presto-main/src/main/java/io/prestosql/util/SliceUtils.java
+++ presto-main/src/main/java/io/prestosql/util/SliceUtils.java
@@ -1,3 +1,16 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.prestosql.util;
 
 import io.airlift.slice.Slice;
@@ -19,7 +32,8 @@ public final class SliceUtils
         String str = slice.toStringUtf8();
         try {
             return Integer.parseInt(str);
-        } catch (final NumberFormatException nfe) {
+        }
+        catch (final NumberFormatException nfe) {
             return defaultInt;
         }
     }
@@ -29,7 +43,8 @@ public final class SliceUtils
         String str = slice.toStringUtf8();
         try {
             return Long.parseLong(str);
-        } catch (final NumberFormatException nfe) {
+        }
+        catch (final NumberFormatException nfe) {
             return defaultLong;
         }
     }
@@ -39,7 +54,8 @@ public final class SliceUtils
         String str = slice.toStringUtf8();
         try {
             return Double.parseDouble(str);
-        } catch (final NumberFormatException nfe) {
+        }
+        catch (final NumberFormatException nfe) {
             return defaultDouble;
         }
     }
